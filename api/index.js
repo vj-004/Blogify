6f46a91fc2e4a7580d5473654fa9416d35a0c774
vj-004 <vj20190204@gmail.com>
2024-07-07 00:40:36 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+
 
 dotenv.config();
 
@@ -17,4 +19,11 @@ const app = express();
 app.listen(3000, () => {
     console.log('Serevr is running on port 3000!!');
 });
+
+app.use('/api/user', userRoutes);
+
+
+
+
+
 
