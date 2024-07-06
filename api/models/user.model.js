52c2c7558f0ba0114a42660cf6d3715db66ed950
vj-004 <vj20190204@gmail.com>
2024-07-07 00:32:55 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import { type } from 'os';
+import { PassThrough } from 'stream';
+
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type: String,
+            required: true,
+            unique: true,
+
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password:{
+            type: String,
+            required: true,
+        }
+    }, {timestamps: true}
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
