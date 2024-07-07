c04b6533da848cf0d65aedab68f1e003178092f2
vj-004 <vj20190204@gmail.com>
2024-07-07 12:54:26 +0530

    added a middle ware and function to handle errors

--- api/index.js
+++ api/index.js
@@ -19,11 +19,22 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log('Serevr is running on port 3000!!');
+    console.log('Server is running on port 3000!!');
 });
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+});
+
 
 
 
