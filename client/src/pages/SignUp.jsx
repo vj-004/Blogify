76868d146ed8a9a86065a4ccc188ba5ecf000793
vj-004 <vj20190204@gmail.com>
2024-07-08 01:03:54 +0530

    added functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,44 @@
-import { Button, Label, TextInput } from 'flowbite-react';
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
 export default function SignUp() {
+  const [formData,setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()});
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false){
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/sign-in');
+      }
+    }catch(err){
+      setErrorMessage(err.message);
+      setLoading(false);
+    }
+  }
   return (
     <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md: items-center gap-5'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md: items-center gap-7'>
       {/* left */}
         <div className='flex-1 '>
         <Link to="/" className='font-bold dark:text-white'>
@@ -14,38 +48,49 @@ export default function SignUp() {
             </span>
         </Link>
         <p className='text-sm mt-5'>
-          This is a demo project. You can signup with your email and password or with Google
+          This is a demo project. You can signup with your email 
+          and password or with Google
         </p>
         </div>
         {/* right  */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username'/>
               <TextInput
                 type='text'
                 placeholder='Username'
                 id = 'username'
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your email'/>
               <TextInput
-                type='text'
+                type='email'
                 placeholder='name@company.com'
                 id = 'email'
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your password'/>
               <TextInput
-                type='passowrd'
+                type='password'
                 placeholder='Password'
                 id = 'password'
+                onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Sign Up
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                  </>
+                ): 'Sign Up'
+              }
             </Button>
           </form>
 
@@ -55,6 +100,13 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
 
         </div>
       </div>
