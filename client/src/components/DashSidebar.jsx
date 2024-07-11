610bea1aef185b62996863d1fc3563284d254b56
vj-004 <vj20190204@gmail.com>
2024-07-11 15:03:47 +0530

    added sign out functionality (only changes to signIn page when you signout from profile though)

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,9 +2,28 @@ import React, { useEffect, useState } from 'react'
 import {Sidebar} from 'flowbite-react'
 import {HiArrowSmRight, HiUser} from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { signoutSuccess } from '../redux/user/userSlice';
 export default function DashSidebar() {
     const location = useLocation();
     const [tab,setTab] = useState('');
+    const dispatch = useDispatch();
+    const handleSignout = async() => {
+      try{
+          const res = await fetch('/api/user/signout',{
+              method: 'POST',
+          });
+          const data = await res.json();
+          if(!res.ok){
+              console.log(data.message);
+          }else{
+              dispatch(signoutSuccess());
+          }
+
+      }catch(error){
+          console.log(error.messgae);
+      }
+  }
     useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get('tab');
@@ -22,8 +41,8 @@ export default function DashSidebar() {
                 </Sidebar.Item>
             </Link>
                 
-                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
-                Sign Out
+                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
+                  Sign Out
                 </Sidebar.Item>
             </Sidebar.ItemGroup>
         </Sidebar.Items>
